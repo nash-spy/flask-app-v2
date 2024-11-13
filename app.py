48154512865d256d7b18993361a17d0154a52aaf
nash-spy <nashleo237@gmail.com>
2024-11-13 06:48:29 -0800

    Error 404 Defect

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/PROJECT-ROUTE')
 def home():
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
