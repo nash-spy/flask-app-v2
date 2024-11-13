0617969d19cd9ca90e95cc7be6e0c199aa91265e
nash-spy <nashleo237@gmail.com>
2024-11-13 07:47:02 -0800

    error 500

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/PROJECT-ROUTE')
 def home():
-    return jsonify(message="Hello level 400 FET, Quality Assurance!")
+     raise Exception("This is a test error for generating a 500 Internal Server Error.")
+    # return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
 
 if __name__ == '__main__':
